f3ea65fc1dc1b05f2905cdad24bb4aaea5449381
qqq-052 <57927622+qqq-052@users.noreply.github.com>
2020-11-22 12:04:29 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export function requset(config) {
     //1.创建axios实例
     const instance = axios.create({
-        baseURL: 'http://152.136.185.210:8000/api/w6',
+        baseURL: 'api',
         timeout: 500
     })
 
